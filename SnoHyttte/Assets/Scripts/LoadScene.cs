0dac1d895f1b4313613a4ec9142c8f446e2b3bc2
Blopikolo <Emil.k.listerud04@gmail.com>
2024-01-21 11:44:50 +0100

    Can Escape

---
+++ SnoHyttte/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadScene : MonoBehaviour
+{
+
+    public bool boksskjekk;
+    public string LevelName;
+    [SerializeField] Text pickUpText;
+   
+    void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+        boksskjekk = false;
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        if (boksskjekk == true && Input.GetKeyDown(KeyCode.E))
+        {
+            //da skjer det ting
+            SceneManager.LoadScene(LevelName); ;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        pickUpText.gameObject.SetActive(true);
+        boksskjekk = true;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        boksskjekk = false;
+        pickUpText.gameObject.SetActive(false);
+    }
+
+
+
+
+
+
+
+
+}
