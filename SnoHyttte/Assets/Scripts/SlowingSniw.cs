bcb2363882f1adb5a7d997ef585833708d3aa644
Blopikolo <Emil.k.listerud04@gmail.com>
2024-01-31 22:53:40 +0100

    34

--- SnoHyttte/Assets/Scripts/SlowingSniw.cs
+++ SnoHyttte/Assets/Scripts/SlowingSniw.cs
@@ -61,8 +61,8 @@ public class TurretSlowMo : MonoBehaviour
     }
         private void OnDrawGizmosSelected()
     {
-        Handles.color = Color.cyan;
-        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+        //Handles.color = Color.cyan;
+        //Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
     }
 
 }
